02ae63c5817b3ea17c324877571849bf49faff3c
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-08 00:59:37 +0300

    using streams

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,17 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 2, 6, 5, 1, 4, 7, 3, 0);
+        numbers.stream()
+                .filter(number -> number % 2 == 1)
+                .sorted()
+                .map(number -> number * 2)
+                .forEach(System.out::println);
     }
 }
